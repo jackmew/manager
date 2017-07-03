2fdcb0307547fce8ea12c7d4dd7feecbaa58523d
Jackmew <wonderfuljack123@gmail.com>
2017-07-03 18:00:24 +0800

    manager done

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,9 @@ import { Actions } from 'react-native-router-flux';
 import {
     EMPLOYEE_UPDATE,
     EMPLOYEE_CREATE,
-    EMPLOYEES_FETCH_SUCCESS
+    EMPLOYEES_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS,
+    EMPLOYEE_DELETE_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -48,5 +50,31 @@ export const employeesFetch = () => {
             //     console.log(snapshot.val());
             //     dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
             // });
+    };
+};
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+    const { currentUser } = firebase.auth();
+
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .set({ name, phone, shift })
+            .then(() => {
+                dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
+                Actions.employeeList({ type: 'reset' }); // remove the back button
+            });
+    };
+};
+
+export const employeeDelete = ({ uid }) => {
+    const { currentUser } = firebase.auth();
+    // no need dispatch, cuz on('value')
+    return (dispatch) => {
+        firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+            .remove()
+            .then(() => {
+                dispatch({ type: EMPLOYEE_DELETE_SUCCESS });
+                Actions.employeeList({ type: 'reset' });
+            });
     };
 };
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,8 @@
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_SAVE_SUCCESS,
+    EMPLOYEE_DELETE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -19,6 +21,10 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, [action.payload.prop]: action.payload.value };
         case EMPLOYEE_CREATE:
             // reset the state
+            return INITIAL_STATE;
+        case EMPLOYEE_SAVE_SUCCESS:
+            return INITIAL_STATE;
+        case EMPLOYEE_DELETE_SUCCESS:
             return INITIAL_STATE;
         default: 
             return state;
